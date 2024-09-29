edc03d41a56c7fec41d697ccf5a5f65133a5c8a3
YavuzSelim <yavuzselimtopal80@gmail.com>
2024-09-29 14:45:20 +0300

    deneme

--- app/src/main/java/com/example/ortakdenemeapp/FeedActivity.java
+++ app/src/main/java/com/example/ortakdenemeapp/FeedActivity.java
@@ -12,7 +12,7 @@ public class FeedActivity extends AppCompatActivity {
 
     private boolean a = true;
     public  boolean osuruk = true;
-    public boolean sicmik = true ;
+    public boolean sicmik = false ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
